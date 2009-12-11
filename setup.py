490a99b3eb6cedc5b688a78d0f4f2dedb6b62473
Sebastien Douche <sebastien.douche@securactive.net>
2009-12-12 00:19:48 +0100

    Prepare release: 0.3.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_version(version):
     except:
         return version
 
-version = '0.4.0dev'
+version = '0.3.1'
 long_description = open('docs/source/overview.txt', 'r').read()
 
 setup(name='sact.recipe.postgresql',
